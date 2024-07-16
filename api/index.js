e9176028d8756e9909c8f9dc4fce521234a8cbc3
Xiao Zhong <zhongxiao2017@gmail.com>
2024-07-16 22:36:00 +1000

    database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("connect mongodb");
+  })
+  .catch((e) => console.log(e));
+
+// const db = mongoose.connection;
+
+// db.on("connected", function () {
+//   console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+// });
 
 const app = express();
 
