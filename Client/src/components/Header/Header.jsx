7242afb217067046dbe17171ff481c2d0f6e0b4f
Xiao Zhong <zhongxiao2017@gmail.com>
2024-07-21 18:36:07 +1000

    add feature handClickOutside button and fix more responsive styling

--- Client/src/components/Header/Header.jsx
+++ Client/src/components/Header/Header.jsx
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./Header.module.scss";
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
+  // menuRef.current is a mutable ref, it can be used to access DOM element or a component instance
+  const menuRef = useRef(null);
 
   const handleShowMenuBtn = () => {
     setShowMenu(!showMenu);
     console.log("menu clicked");
   };
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      // check if mouse clicks elements not be useReffed (anywhere on the page except the referenced element), if so, set showMenu false
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+    //and trigger the handleClickOutside function by mousedown -- mousedown is press down. Can use click too, click is press down and release,
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // clean side effect by using return function when the component unmounts.
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   console.log(showMenu);
 
@@ -26,7 +43,8 @@ const Header = () => {
         <span className={styles.span}>Movies</span>{" "}
       </NavLink> */}
 
-      <div className={styles.menuWrapper}>
+      {/* add ref={menuRef} here for the menuWrapper to access it's DOM */}
+      <div className={styles.menuWrapper} ref={menuRef}>
         <div className={styles.links}>
           <NavLink to="/my-movies" className={styles.link}>
             <span className={styles.collectionSpan}>Collection</span>
